2f3c4b802b5a5655293fafaaf7377af8a229c93f
unknown <raul@laxara.com>
2025-01-22 02:32:48 +0100

    Tres cicles nous amd degradació

--- keyboards/keychron/v5_max/iso_encoder/keymaps/via/rgb_cicle_cerberos.h
+++ keyboards/keychron/v5_max/iso_encoder/keymaps/via/rgb_cicle_cerberos.h
@@ -45,30 +45,75 @@ void RGB_CICLE_CERBEROS(bool inactiu, uint8_t color_1, uint8_t color_2, uint8_t 
     // Si estem en modo inactiu, entonces pintem nosaltres, si no deixarem el automatic que estiga vigent
         // apaguem totes les tecles
         rgb_matrix_set_color_all(0,0,0); 
-        
+
         // Perro 1
         rgb_matrix_set_color(
             circuit_circular[posicio_1],
-            rgb_colors[color_1][0],
-            rgb_colors[color_1][1],
-            rgb_colors[color_1][2]
+            rgb_colors[color_1][0] * 0.1,
+            rgb_colors[color_1][1] * 0.1,
+            rgb_colors[color_1][2] * 0.1
         );
+
+        rgb_matrix_set_color(
+            circuit_circular[(posicio_1 + 1) % TOTAL_CIRCUIT_CIRCULAR],
+            rgb_colors[color_1][0] * 0.4,
+            rgb_colors[color_1][1] * 0.4,
+            rgb_colors[color_1][2] * 0.4
+        );
+
+        rgb_matrix_set_color(
+            circuit_circular[(posicio_1 + 2) % TOTAL_CIRCUIT_CIRCULAR],
+            rgb_colors[color_1][0] * 1,
+            rgb_colors[color_1][1] * 1,
+            rgb_colors[color_1][2] * 1
+        );
+
 
         // Perro 2
         rgb_matrix_set_color(
             circuit_circular[posicio_2],
-            rgb_colors[color_2][0],
-            rgb_colors[color_2][1],
-            rgb_colors[color_2][2]
+            rgb_colors[color_2][0] * 0.1,
+            rgb_colors[color_2][1] * 0.1,
+            rgb_colors[color_2][2] * 0.1
+
+        );
+
+        rgb_matrix_set_color(
+            circuit_circular[(posicio_2 + 1) % TOTAL_CIRCUIT_CIRCULAR],
+            rgb_colors[color_2][0] * 0.4,
+            rgb_colors[color_2][1] * 0.4,
+            rgb_colors[color_2][2] * 0.4
+
+        );
+
+        rgb_matrix_set_color(
+            circuit_circular[(posicio_2 + 2) % TOTAL_CIRCUIT_CIRCULAR],
+            rgb_colors[color_2][0] * 1,
+            rgb_colors[color_2][1] * 1,
+            rgb_colors[color_2][2] * 1
 
         );
 
         // Perro 3
         rgb_matrix_set_color(
             circuit_circular[posicio_3],
-            rgb_colors[color_3][0],
-            rgb_colors[color_3][1],
-            rgb_colors[color_3][2]
+            rgb_colors[color_3][0] * 0.1,
+            rgb_colors[color_3][1] * 0.1,
+            rgb_colors[color_3][2] * 0.1
+        );
+
+        rgb_matrix_set_color(
+            circuit_circular[(posicio_3 + 1) % TOTAL_CIRCUIT_CIRCULAR],
+            rgb_colors[color_3][0] * 0.4,
+            rgb_colors[color_3][1] * 0.4,
+            rgb_colors[color_3][2] * 0.4
+        );
+
+        rgb_matrix_set_color(
+            circuit_circular[(posicio_3 + 2) % TOTAL_CIRCUIT_CIRCULAR],
+            rgb_colors[color_3][0] * 1,
+            rgb_colors[color_3][1] * 1,
+            rgb_colors[color_3][2] * 1
         );
 
 
